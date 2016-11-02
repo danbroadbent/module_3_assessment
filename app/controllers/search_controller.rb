e5e5e049ce10b5e284dcfd84b92a38de3158bad9
Daniel Broadbent <dbroadbent85@gmail.com>
2016-11-02 10:43:58 -0600

    add faraday api connection

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
   def index
     conn = Faraday.new(:url => 'https://api.bestbuy.com')
     response = conn.get "/v1/stores(area(#{params['q']},25))?format=json&show=storeId,storeType,name&pageSize=15&apiKey=#{ENV['bestbuy_api_key']}"
-    byebug
+    @results = JSON.parse(response.body)
+    # byebug
   end
 end
