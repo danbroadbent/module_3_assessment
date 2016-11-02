f6117431bba7a19e48fd37ef285cd3336a9053a8
Daniel Broadbent <dbroadbent85@gmail.com>
2016-11-02 09:29:22 -0600

    add api endpoint for items index

--- spec/requests/api/v1/items_get_spec.rb
+++ spec/requests/api/v1/items_get_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe 'api returns a list of items' do
   it 'get request to items endpoint' do
+    Item.create(id: 1, name: 'user 1', description: 'description 1', image_url: 'image.url')
+    Item.create(id: 2, name: 'user 2', description: 'description 2', image_url: 'image2.url')
+    Item.create(id: 3, name: 'user 3', description: 'description 3', image_url: 'image3.url')
     get '/api/v1/items'
-    item.new(id: 1, name: 'user 1', description: 'description 1', image_url: 'image.url')
-    item.new(id: 2, name: 'user 2', description: 'description 2', image_url: 'image2.url')
-    item.new(id: 3, name: 'user 3', description: 'description 3', image_url: 'image3.url')
     items = JSON.parse(response.body)
-    expect(response_status).to eq(200)
+    expect(response.status).to eq(200)
     # expect(response.class).to eq(JSON)
     expect(items.count).to eq(3)
     expect(items[0].id).to eq(1)
