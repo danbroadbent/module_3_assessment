e39a28056a58f412e6755a0bb7d7fc1e19724a16
Daniel Broadbent <dbroadbent85@gmail.com>
2016-11-02 12:04:21 -0600

    add model test criteria

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe 'nearby location by zip returns locations instances' do
   it 'takes zip returns locations' do
-    
+    locations = Location.nearby_locations('80202')
+    expect(locations.class).to eq(Array)
+    expect(locations[0].class).to eq(Location)
   end
 end
